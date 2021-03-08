bf35b077dfbb58ec98cb74dfab5203bda93ba22c
Rulla <rulla@Rullas-MacBook-Pro-2.local>
2021-03-08 10:33:32 -0800

    adding files

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Day-Planner
-Planner to organize daily tasks and activities
+# Summary:
+This is a planner application that allows users to save events for each hour of the day. 
+
+# Technologies used:
+- HTML
+- CSS
+- Javascript
+
+# Deployed links:
+
+
+ 
+
+ # Screenshot:
+
+![Screenshot of Coding Quiz](assets/screenshot.png)
---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function (){
+
+
+//create date and append to html page
+var today=moment().format('MMMM Do YYYY');
+console.log(today)
+$("#currentDay").append(today);
+var saveBtn=$(".saveBtn")
+
+saveBtn.on("click", saveValue )
+
+function saveValue (){
+var inputValue= $(this).siblings(".timeValue").val()
+console.log(inputValue)
+var key=$(this).parent().attr("value")
+console.log(key)
+localStorage.setItem(key,inputValue)
+}
+function getValue(){
+    var timeBlock=$(".time-block")
+    timeBlock.each(function(){
+        var value=$(this).attr("value")
+        var getLocalStorage=localStorage.getItem(value)
+        $(this).children(".timeValue").val(getLocalStorage)
+    })
+}
+getValue()
+})
